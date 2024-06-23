8595dabae7eb8efcf6b02404e41739ce67cc7466
Arthi Selavarj <164236172+02ARTHI@users.noreply.github.com>
2024-06-23 23:58:26 +0530

    Add files via upload

--- ListTest.java
+++ ListTest.java
@@ -8,5 +8,12 @@ public class ListTest {
     obj.add(1);
     obj.add(2);
     System.out.println(obj);
+
+    List<Integer> obj1 = new ArrayList<Integer>();
+    obj1.addAll(obj);
+    System.out.println(obj1);
+
+    List<Integer> obj3 = new ArrayList<Integer>(obj1);
+    System.out.println(obj3);
    }
 }
